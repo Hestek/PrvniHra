b5c6e08e899ab66434958350205df0840e95bd9e
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2022-03-29 11:20:17 +0200

    Přidejte soubory projektu.

---
+++ PrvniHra/Character.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrvniHra
+{
+    class Character
+    {
+        private int shield;
+        private int hp;
+        private int damage;
+
+        public string Name { get; }
+        public int HP { get=> hp;  }
+        public int Shield { get=>shield; }
+        public int Damage { get=>damage;  }
+
+        public Character(string name)
+        {
+            Name = name;
+            hp = 100;
+            shield = 50;
+            damage = 20;
+        }
+
+        public void GetHit(int damage)
+        {
+            shield -= damage;
+            if(shield < 0)
+            {
+                hp -= shield;
+                shield = 0;
+            }
+                        
+
+        }
+    }
+
+}
---
+++ PrvniHra/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrvniHra
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Character character1;
+        Character character2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            character1 = new Character("Petr");
+            character2 = new Character("Pavel");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
